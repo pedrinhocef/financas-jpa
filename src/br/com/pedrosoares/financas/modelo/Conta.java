7bcca6929e01ac8975476cea8807cfe1df581921
Pedro Soares <pedrinhocef@gmail.com>
2018-05-10 21:23:19 -0300

    first commit

---
+++ src/br/com/pedrosoares/financas/modelo/Conta.java
@@ -0,0 +1,14 @@
+package br.com.pedrosoares.financas.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Conta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+}
